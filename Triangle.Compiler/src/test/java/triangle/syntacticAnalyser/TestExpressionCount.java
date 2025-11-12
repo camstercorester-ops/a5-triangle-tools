8462e34985780090887eb0c367afad0a9adb4764
Lisa M <camstercorester@gmail.com>
2025-11-12 15:27:06 +0000

    Completed task 6 extra test added

--- Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestExpressionCount.java
+++ Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestExpressionCount.java
@@ -8,7 +8,7 @@ import triangle.Compiler;
 
 public class TestExpressionCount {
     @Test
-    public void testCharacterExpressionCount(){
+    public void testIntegerExpressionCount(){
         Compiler compiler = new Compiler();
         PrintStream stdout = System.out;
 
@@ -26,5 +26,27 @@ public class TestExpressionCount {
 
         String capturedOut = outputStream.toString();
         System.out.println(capturedOut);
-    }  
+    }
+    
+    @Test
+    public void testCharacterExpressionCount(){
+        Compiler compiler = new Compiler();
+        PrintStream stdout = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream output = new PrintStream(outputStream);
+
+        System.setOut(output);
+
+        String path = "..\\programs\\names.tri";
+        compiler.showStats = true;
+        
+        Compiler.compileProgram(path, "obj.tam", false, false, compiler);
+
+        System.setOut(stdout);
+
+        String capturedOut = outputStream.toString();
+        System.out.println(capturedOut);
+    }
+
 }
